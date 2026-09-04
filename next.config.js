b7316b44e7bdcc172056ecfbd676b30f74f09e80
Gregory Dennis Putnam <213495108+mmmm013@users.noreply.github.com>
2026-09-04 17:01:50 -0500

    build: trace ffmpeg binary into KKr prosecution function

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
   outputFileTracingRoot: require('path').join(__dirname),
+  outputFileTracingIncludes: {
+    '/api/admin/kkr-torn-memories/prosecute': ['./node_modules/ffmpeg-static/ffmpeg'],
+  },
 };
 
 module.exports = nextConfig;
